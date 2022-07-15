07e7c19d0c245d7ea5ccde73879eaf18de33a21b
cenfun <cenfun@qq.com>
2022-07-15 19:02:34 +0800

    update layout

--- packages/vine-ui/src/components/layout/layout.vue
+++ packages/vine-ui/src/components/layout/layout.vue
@@ -1,247 +1,247 @@
 <template>
   <div
+    ref="el"
     :class="classList"
     :style="styleList"
   >
     <slot>
-      <BaseRender :content="content" />
+      <BaseRender :content="props.content" />
     </slot>
   </div>
 </template>
 
-<script>
-import Base from '../../base/base.vue';
+<script setup>
 import {
-    unbindEvents, bindEvents, preventDefault
-} from '../../util/util.js';
+    computed, onMounted, onUnmounted, ref
+} from 'vue';
+import { useBase, BaseRender } from '../../base/base.js';
 
-export default {
+import Util from '../../util/util.js';
 
-    name: 'VuiLayout',
+const { cid } = useBase('VuiLayout');
 
-    extends: Base,
-
-    props: {
-        width: {
-            type: String,
-            default: ''
-        },
-        height: {
-            type: String,
-            default: ''
-        },
-        direction: {
-            type: String,
-            default: 'row',
-            validator(value) {
-                return ['row', 'column'].includes(value);
-            }
-        },
-        size: {
-            type: String,
-            default: '5px'
-        },
-        content: {
-            validator: (v) => true,
-            default: ''
+const props = defineProps({
+    width: {
+        type: String,
+        default: ''
+    },
+    height: {
+        type: String,
+        default: ''
+    },
+    direction: {
+        type: String,
+        default: 'row',
+        validator(value) {
+            return ['row', 'column'].includes(value);
         }
     },
-
-    computed: {
-        classList() {
-            const ls = [
-                'vui',
-                'vui-layout',
-                `vui-layout-${this.direction}`,
-                `vui-flex-${this.direction}`
-            ];
-            ls.push(this.cid);
-            return ls;
-        },
-
-        styleList() {
-            const st = {};
-            if (this.width) {
-                st.width = this.width;
-            }
-            if (this.height) {
-                st.height = this.height;
-            }
-            if (this.size) {
-                st['--vui-gutter-size'] = this.size;
-            }
-            return st;
-        }
+    size: {
+        type: String,
+        default: '5px'
     },
+    content: {
+        validator: (v) => true,
+        default: ''
+    }
+});
 
-    mounted() {
-        const children = Array.from(this.$el.children);
-        // console.log(children);
-        if (children.length < 2) {
-            return;
-        }
-        children.length -= 1;
-        children.forEach((child, i) => {
-            const gutter = document.createElement('div');
-            gutter.className = 'vui-layout-gutter';
-            child.insertAdjacentElement('afterend', gutter);
-        });
+const classList = computed(() => {
+    const ls = [
+        'vui',
+        'vui-layout',
+        `vui-layout-${props.direction}`,
+        `vui-flex-${props.direction}`
+    ];
+    ls.push(cid);
+    return ls;
+});
 
-        this.bindEvents();
+const styleList = computed(() => {
+    const st = {};
+    if (props.width) {
+        st.width = props.width;
+    }
+    if (props.height) {
+        st.height = props.height;
+    }
+    if (props.size) {
+        st['--vui-gutter-size'] = props.size;
+    }
+    return st;
+});
 
-    },
+const el = ref(null);
+let $el;
+let gutterCL;
+let previousIframe;
+let option;
 
-    methods: {
-        bindEvents() {
-            this.mousedownEvents = {
-                mousedown: {
-                    handler: (e) => {
-                        this.startHandler(e);
-                    }
-                }
-            };
-            bindEvents(this.mousedownEvents, this.$el);
-        },
+let prevNode;
+let nextNode;
 
-        startHandler(e) {
+let prevSize;
+let nextSize;
 
-            const cl = e.target.classList;
-            if (!cl.contains('vui-layout-gutter')) {
-                return;
-            }
-
-            cl.add('vui-layout-active');
-            this.$el.classList.add('vui-layout-moving');
-
-            this.initNodes(e);
-
-            this.option = {
-                startX: e.pageX,
-                startY: e.pageY,
-                offsetX: 0,
-                offsetY: 0
-            };
-
-            //stop nested layout event
-            e.stopImmediatePropagation();
-            //console.log('start');
-
-            this.windowEvents = {
-                mousemove: {
-                    handler: (ee) => {
-                        this.iframeHandler(ee);
-                        this.mouseMoveHandler(ee);
-                    },
-                    options: true
-                },
-                mouseup: {
-                    handler: (ee) => {
-                        cl.remove('vui-layout-active');
-                        this.mouseUpHandler(ee);
-                    },
-                    options: {
-                        once: true
-                    }
-                }
-            };
-            bindEvents(this.windowEvents, window);
-        },
-
-        initNodes(e) {
-            this.prevNode = null;
-            this.nextNode = null;
-
-            const prevNode = e.target.previousElementSibling;
-            const nextNode = e.target.nextElementSibling;
-
-            // const prevStyle = getComputedStyle(prevNode);
-            // const nextStyle = getComputedStyle(nextNode);
-            // console.log('prev:', prevStyle.flexBasis, 'next:', nextStyle.flexBasis);
-
-            const sizeKey = this.direction === 'row' ? 'clientWidth' : 'clientHeight';
-
-            if (prevNode) {
-                this.prevSize = prevNode[sizeKey];
-                this.prevNode = prevNode;
-            }
-
-            if (nextNode) {
-                this.nextSize = nextNode[sizeKey];
-                this.nextNode = nextNode;
-            }
-        },
-
-        clean() {
-            unbindEvents(this.windowEvents);
-            this.windowEvents = null;
-            if (this.previousIframe) {
-                this.previousIframe.classList.remove('iframe-pointer-events-none');
-                this.previousIframe = null;
-            }
-            this.$el.classList.remove('vui-layout-moving');
-            this.prevNode = null;
-            this.nextNode = null;
-        },
-
-        iframeHandler: function(e) {
-            const target = e.target;
-            if (target.nodeName !== 'IFRAME') {
-                return;
-            }
-            if (target === this.previousIframe) {
-                return;
-            }
-            if (this.previousIframe) {
-                this.previousIframe.classList.remove('iframe-pointer-events-none');
-            }
-            target.classList.add('iframe-pointer-events-none');
-            this.previousIframe = target;
-        },
-
-        mouseMoveHandler: function(e) {
-            preventDefault(e);
-            const o = this.option;
-            o.offsetX = e.pageX - o.startX;
-            o.offsetY = e.pageY - o.startY;
-            this.updateHandler(e);
-        },
-
-        mouseUpHandler: function(e) {
-            preventDefault(e);
-            this.clean();
-        },
-
-        updateHandler(e) {
-            const offsetKey = this.direction === 'row' ? 'offsetX' : 'offsetY';
-            const offset = this.option[offsetKey];
-
-            //console.log(offset);
-            //console.log(this.prevNode, this.nextNode);
-
-            const prevSize = this.prevSize + offset;
-            const nextSize = this.nextSize - offset;
-            if (prevSize < 0 || nextSize < 0) {
-                return;
-            }
-
-            //const styleKey = this.direction === 'row' ? 'width' : 'height';
-            const styleKey = 'flexBasis';
-            if (this.prevNode) {
-                this.prevNode.style[styleKey] = `${prevSize}px`;
-            }
-            if (this.nextNode) {
-                this.nextNode.style[styleKey] = `${nextSize}px`;
-            }
-
-        },
-
-        beforeUnmount() {
-            this.clean();
-            unbindEvents(this.mousedownEvents);
-            this.mousedownEvents = null;
+const mousedownEvents = {
+    mousedown: {
+        handler: (e) => {
+            startHandler(e);
         }
     }
 };
+
+const windowEvents = {
+    mousemove: {
+        handler: (ee) => {
+            iframeHandler(ee);
+            mouseMoveHandler(ee);
+        },
+        options: true
+    },
+    mouseup: {
+        handler: (ee) => {
+            gutterCL.remove('vui-layout-active');
+            gutterCL = null;
+            mouseUpHandler(ee);
+        },
+        options: {
+            once: true
+        }
+    }
+};
+
+const startHandler = (e) => {
+
+    gutterCL = e.target.classList;
+    if (!gutterCL.contains('vui-layout-gutter')) {
+        return;
+    }
+
+    gutterCL.add('vui-layout-active');
+
+    $el.classList.add('vui-layout-moving');
+
+    initNodes(e);
+
+    option = {
+        startX: e.pageX,
+        startY: e.pageY,
+        offsetX: 0,
+        offsetY: 0
+    };
+
+    //stop nested layout event
+    e.stopImmediatePropagation();
+    //console.log('start');
+    Util.bindEvents(windowEvents, window);
+};
+
+const initNodes = (e) => {
+    prevNode = null;
+    nextNode = null;
+
+    const _prevNode = e.target.previousElementSibling;
+    const _nextNode = e.target.nextElementSibling;
+
+    // const prevStyle = getComputedStyle(_prevNode);
+    // const nextStyle = getComputedStyle(_nextNode);
+    // console.log('prev:', prevStyle, 'next:', nextStyle);
+
+    const sizeKey = props.direction === 'row' ? 'clientWidth' : 'clientHeight';
+
+    if (_prevNode) {
+        prevSize = _prevNode[sizeKey];
+        prevNode = _prevNode;
+    }
+
+    if (_nextNode) {
+        nextSize = _nextNode[sizeKey];
+        nextNode = _nextNode;
+    }
+};
+
+const iframeHandler = function(e) {
+    const target = e.target;
+    if (target.nodeName !== 'IFRAME') {
+        return;
+    }
+    if (target === previousIframe) {
+        return;
+    }
+    if (previousIframe) {
+        previousIframe.classList.remove('iframe-pointer-events-none');
+    }
+    target.classList.add('iframe-pointer-events-none');
+    previousIframe = target;
+};
+
+const mouseMoveHandler = function(e) {
+    Util.preventDefault(e);
+    option.offsetX = e.pageX - option.startX;
+    option.offsetY = e.pageY - option.startY;
+    updateHandler(e);
+};
+
+const mouseUpHandler = function(e) {
+    Util.preventDefault(e);
+    clean();
+};
+
+const updateHandler = (e) => {
+
+    const offsetKey = props.direction === 'row' ? 'offsetX' : 'offsetY';
+    const offset = option[offsetKey];
+
+    //console.log(offset);
+
+    const _prevSize = prevSize + offset;
+    const _nextSize = nextSize - offset;
+    if (_prevSize < 0 || _nextSize < 0) {
+        return;
+    }
+
+    if (prevNode) {
+        prevNode.style.flexBasis = `${_prevSize}px`;
+    }
+    if (nextNode) {
+        nextNode.style.flexBasis = `${_nextSize}px`;
+    }
+
+};
+
+const clean = () => {
+    Util.unbindEvents(windowEvents);
+    if (previousIframe) {
+        previousIframe.classList.remove('iframe-pointer-events-none');
+        previousIframe = null;
+    }
+    $el.classList.remove('vui-layout-moving');
+    prevNode = null;
+    nextNode = null;
+};
+
+onMounted(() => {
+    $el = el.value;
+    const children = Array.from($el.children);
+    // console.log(children);
+    if (children.length < 2) {
+        return;
+    }
+    children.length -= 1;
+    children.forEach((child, i) => {
+        const gutter = document.createElement('div');
+        gutter.className = 'vui-layout-gutter';
+        child.insertAdjacentElement('afterend', gutter);
+    });
+
+    Util.bindEvents(mousedownEvents, $el);
+});
+
+onUnmounted(() => {
+    clean();
+    Util.unbindEvents(mousedownEvents);
+});
 
 </script>
 
