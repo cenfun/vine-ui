3405bfb239107ff992ceea795420ad039733f590
cenfun <cenfun@gmail.com>
2023-03-29 20:09:46 +0800

    update tooltip

--- packages/vine-ui/src/components/tooltip/tooltip.vue
+++ packages/vine-ui/src/components/tooltip/tooltip.vue
@@ -12,13 +12,16 @@
 </template>
 <script setup>
 import {
-    computed, onMounted, reactive, ref, watch, watchEffect, nextTick
+    computed, onMounted, onUnmounted, reactive, ref, watch, watchEffect
 } from 'vue';
+
 import { useBase } from '../../base/base.js';
 
-import { getBestPosition, getRect } from '../../base/base-popup.js';
+import {
+    getBestPosition, getPositionStyle, getRect
+} from 'popover-helper';
 
-import { autoPx } from '../../utils/util.js';
+import { autoPx, microtask } from '../../utils/util.js';
 
 const { cid } = useBase('VuiTooltip');
 
@@ -71,6 +74,11 @@ const props = defineProps({
         default: ''
     },
 
+    nonreactive: {
+        type: Boolean,
+        default: true
+    },
+
     visible: {
         type: Boolean,
         default: true
@@ -83,10 +91,10 @@ let $el;
 
 const data = reactive({
     visible: props.visible,
-    position: 'top',
-    align: 'center',
+
     top: 0,
-    left: 0
+    left: 0,
+    background: ''
 });
 
 watchEffect(() => {
@@ -94,47 +102,33 @@ watchEffect(() => {
 });
 
 const classList = computed(() => {
-    return [
+    const ls = [
         'vui',
-        'vui-popup',
-        `vui-popup-${data.position}`,
-        `vui-popup-${data.position}-${data.align}`,
         'vui-tooltip',
         cid
     ];
+    if (props.nonreactive) {
+        ls.push('vui-tooltip-nonreactive');
+    }
+    return ls;
 });
 
 const styleList = computed(() => {
     const st = {
         top: `${data.top}px`,
         left: `${data.left}px`,
-        'max-width': autoPx(props.maxWidth)
+        background: data.background
     };
-    if (props.borderColor) {
-        st['--vui-popup-border-color'] = props.borderColor;
-    }
-    if (props.bgColor) {
-        st['--vui-popup-bg-color'] = props.bgColor;
-    }
     if (props.color) {
-        st['--vui-popup-color'] = props.color;
+        st['--vui-tooltip-color'] = props.color;
+    }
+    if (props.maxWidth) {
+        st['--vui-tooltip-max-width'] = autoPx(props.maxWidth);
     }
     return st;
 });
 
 // ====================================================================================================
-
-watch(() => data.visible, () => {
-    update();
-});
-
-const update = () => {
-    contentHandler();
-    // do not setTimeout, because we can see popover top left in first time
-    nextTick(() => {
-        updateSync();
-    });
-};
 
 const updateSync = () => {
     if (!data.visible) {
@@ -142,78 +136,103 @@ const updateSync = () => {
     }
     const containerRect = getRect(props.container || window);
     const targetRect = getRect(props.target);
-
-    // fix for arrow size
-    const arrowSize = 10;
-    targetRect.left -= arrowSize;
-    targetRect.top -= arrowSize;
-    targetRect.width += arrowSize * 2;
-    targetRect.height += arrowSize * 2;
-
-    const rect = getRect(`.${cid}`);
+    const tooltipRect = getRect(`.${cid}`);
     const positions = props.positions;
 
-    // console.log(containerRect, targetRect, rect);
+    // console.log(containerRect, targetRect, tooltipRect);
 
     const positionInfo = getBestPosition(
         containerRect,
         targetRect,
-        rect,
+        tooltipRect,
         positions
     );
 
-    // no change
-    if (data.position === positionInfo.position
-            && data.align === positionInfo.align
-            && data.top === positionInfo.top
-            && data.left === positionInfo.left) {
-        return;
-    }
+    const style = getPositionStyle(positionInfo, {
+        bgColor: props.bgColor,
+        borderColor: props.borderColor
+    });
 
-    // console.log(positionInfo);
-
-    data.position = positionInfo.position;
-    data.align = positionInfo.align;
     data.top = positionInfo.top;
     data.left = positionInfo.left;
+    data.background = style.background;
 };
 
-const contentHandler = () => {
+const update = microtask(updateSync);
+
+const render = () => {
+    if (!data.visible) {
+        unbindContainerEvent();
+        return;
+    }
     if (!$el) {
         return;
     }
+    bindContainerEvent();
     const $content = $el.querySelector('.vui-tooltip-content');
     if (props.html) {
         $content.innerHTML = props.text;
-        return;
+    } else {
+        $content.innerText = props.text;
     }
-    $content.innerText = props.text;
+    update();
+};
+
+
+let resizeObserver;
+const bindContainerEvent = () => {
+    resizeObserver = new ResizeObserver((entries) => {
+        update();
+    });
+    resizeObserver.observe($el);
+};
+
+const unbindContainerEvent = () => {
+    if (resizeObserver) {
+        resizeObserver.disconnect();
+        resizeObserver = null;
+    }
 };
 
 // ====================================================================================================
 
+watch(() => data.visible, (v) => {
+    render();
+});
+
 onMounted(() => {
     $el = el.value;
-    update();
+    render();
+});
+
+onUnmounted(() => {
+    unbindContainerEvent();
 });
 
 </script>
 
 <style lang="scss">
 .vui-tooltip {
+    --vui-tooltip-color: inherit;
+    --vui-tooltip-max-width: 320px;
+
     position: fixed;
     z-index: 1000;
     margin: 0;
-    pointer-events: none;
+    padding: 20px;
+    box-sizing: border-box;
+    color: var(--vui-popover-color);
+    border: none;
+    filter: drop-shadow(1px 2px 2px rgb(0 0 0 / 20%));
+
+    &.vui-tooltip-nonreactive {
+        pointer-events: none;
+    }
 
     .vui-tooltip-content {
-        display: inline-block;
-        padding: 10px;
-        //IE https://caniuse.com/#feat=wordwrap
-        word-wrap: break-word;
-        //https://developer.mozilla.org/zh-CN/docs/Web/CSS/word-break
-        word-break: break-word;
-        overflow-wrap: break-word;
+        position: relative;
+        max-width: var(--vui-tooltip-max-width);
+        overflow: hidden;
     }
 }
 </style>
